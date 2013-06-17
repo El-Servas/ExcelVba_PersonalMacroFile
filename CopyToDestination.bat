009150964868d97038eb9817ba7b262af78ad2e9
MAS <a3044386>
2013-06-17 10:15:44 -0500

    Extiende el archivo que copia los archivos a sus ubicaciones finales para que también copie las librerías.
    Se agregó un archivo con los strings que el XCOPY ignora al momento de copiar las librerías.

--- CopyToDestination.bat
+++ CopyToDestination.bat
@@ -1,10 +1,36 @@
 @echo off
+echo ==============================================================================
+echo Se copiarán los archivos de macros personales y de librerías a las ubicaciones
+echo estándar.
+echo.
+echo Cualquier tecla para continuar, Ctrl-C para cancelar.
+echo ==============================================================================
+pause
+
+echo ==============================================================================
 echo Copiando Archivo Personal de Macros a:
 echo %appdata%\Microsoft\Excel\XLSTART...
+echo.
+
 copy XLSTART\PersonalDev.* Personal.* > nul
 del *.TXTXL.TXT
 copy "Personal.*" "%appdata%\Microsoft\Excel\XLSTART" > nul
 del Personal.*
+
 echo Listo.
+echo.
+
+echo ==============================================================================
 echo DENTRO DE EXCEL, RE-ASIGNAR LOS ACCESOS DIRECTOS A LAS MACROS QUE LAS TUVIERAN
+echo ==============================================================================
+echo.
+
+echo ------------------------------------------------------------------------------
+echo Copiando librerias...
+echo.
+
+XCOPY MyLibs\*.* %appdata%\Microsoft\Excel\XLSTART\MyLibs /I /S /Y /Q /EXCLUDE:xcopy.ignore
+
+echo Listo.
+echo.
 pause
